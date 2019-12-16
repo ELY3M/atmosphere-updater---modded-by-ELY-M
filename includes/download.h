02bef54e69865b1a3f580bb17d0b6c89f668f0c3
ELY M <elymbmx@gmail.com>
2019-12-16 04:44:11 -0600

    updater

--- includes/download.h
+++ includes/download.h
@@ -3,7 +3,7 @@
 
 #define AMS_URL     "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases"
 #define HEKATE_URL  "https://api.github.com/repos/CTCaer/hekate/releases/latest"
-#define APP_URL     "https://github.com/ITotalJustice/atmosphere-updater/releases/latest/download/atmosphere-updater.nro"
+#define APP_URL     "https://github.com/ELY3M/atmosphere-updater---modded-by-ELY-M/releases/latest/download/atmosphere-updater.nro"
 
 int downloadFile(const char *url, const char *output, int api_mode);
 
